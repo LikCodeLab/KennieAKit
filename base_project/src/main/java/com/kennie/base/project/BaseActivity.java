cd385d8dabe0d2e92a5a3ce5cba70610aa5c1093
kennie <245794335@qq.com>
2022-01-10 17:37:33 +0800

    update

---
+++ base_project/src/main/java/com/kennie/base/project/BaseActivity.java
@@ -0,0 +1,17 @@
+package com.kennie.base.project;
+
+import android.os.Bundle;
+
+import com.kennie.base.project.core.AbstractActivity;
+
+
+/**
+ * 所有项目Activity基类
+ */
+public abstract class BaseActivity extends AbstractActivity {
+
+    @Override
+    protected void init(Bundle savedInstanceState) {
+
+    }
+}
